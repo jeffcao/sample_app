5dc8b3ef849a310043c994d8f81485c65d4448e8
jeff <jeff.cao.cn@gmail.com>
2013-01-12 18:05:21 +0800

    Finished layout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
 
 
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -10,7 +12,7 @@ SampleApp::Application.routes.draw do
   get "pages/help"
 
 match '/', :to => 'pages#home'
-
+match '/signup',:to => 'users#new'
 match '/contact', :to => 'pages#contact'
 match '/about',:to => 'pages#about'
 match '/help',:to => 'pages#help'
